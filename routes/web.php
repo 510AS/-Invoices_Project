67dc9bd7527316a79b35fb3fcc22b969a56f1280
Ahmed Shehata <a.shehata.mahrus@gmail.com>
2021-12-05 23:41:55 +0200

    invoices mc

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('invoices','InvoiceController' );
+
+Route::resource('sections', 'SectionsController');
+
+Route::resource('products', 'ProductsController');
+
+Route::get('/section/{id}', 'InvoiceController@getproducts');
+
+Route::get('/{page}', 'AdminController@index');
